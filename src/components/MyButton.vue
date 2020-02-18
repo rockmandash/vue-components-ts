8d581323f7ccef80d13ace608bc7e05ec15c7e15
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-02-18 13:49:57 +0800

    update

---
+++ src/components/MyButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="button is-primary" @click="onClick">
+    <span>Cool</span>
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "my-button",
+  methods: {
+    onClick() {
+      this.$emit("click");
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+button {
+  border: 1px solid #eee;
+  border-radius: 3px;
+  background-color: #ffffff;
+  cursor: pointer;
+  font-size: 15pt;
+  padding: 3px 10px;
+  margin: 10px;
+
+  span {
+    font-size: 30px;
+  }
+}
+</style>
