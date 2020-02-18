60f2bd25ec54d0b2ce6c28dd6625f4092b59d3e5
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-02-18 14:49:06 +0800

    Update makeRootIndexFile.js

--- scripts/makeRootIndexFile.js
+++ scripts/makeRootIndexFile.js
@@ -1,17 +1,15 @@
-const fs = require('fs-extra');
-const { allUtilIndexFilePaths, getParentFolderName } = require('./utils');
-const paths = require('./paths');
+const fs = require("fs-extra");
+const { allUtilIndexFilePaths, getParentFolderName } = require("./utils");
+const paths = require("./paths");
 
 fs.removeSync(paths.rootIndexFilePath);
 
 const rootIndexFileData = allUtilIndexFilePaths
   .map(utilIndexFilePath => {
     const utilName = getParentFolderName(utilIndexFilePath);
-    const fromPath = utilIndexFilePath
-      .replace(paths.srcFolder, '.')
-      .replace('/index.vue', '');
+    const fromPath = utilIndexFilePath.replace(paths.srcFolder, ".");
     return `export { default as ${utilName} } from '${fromPath}';`;
   })
-  .join('\n');
+  .join("\n");
 
 fs.writeFileSync(paths.rootIndexFilePath, rootIndexFileData);
