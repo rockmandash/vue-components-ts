709fe996aa643fe5af41003980b7eed3b1c1fac5
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-02-19 14:16:20 +0800

    update

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -3,9 +3,13 @@ import { addParameters } from '@storybook/vue';
 
 addParameters({
   backgrounds: [
-    { name: 'white', value: '#ffffff', default: true },
     { name: 'black', value: '#000000' },
     { name: 'twitter', value: '#00aced' },
     { name: 'facebook', value: '#3b5998' }
+  ],
+  paddings: [
+    { name: 'Small', value: '16px' },
+    { name: 'Medium', value: '32px' },
+    { name: 'Large', value: '64px' }
   ]
 });
