86e1868d835d322fdc6e67e16355c29f7f0b79a2
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-02-19 15:20:08 +0800

    Update makeType.js

--- scripts/makeType.js
+++ scripts/makeType.js
@@ -3,13 +3,15 @@ const path = require('path');
 const { allUtilIndexFilePaths, getParentFolderName } = require('./utils');
 const paths = require('./paths');
 
-const typeFileData = `import Vue from 'vue';` + '\n';
-allUtilIndexFilePaths
-  .map(utilIndexFilePath => {
-    const utilName = getParentFolderName(utilIndexFilePath);
-    return `export declare const ${utilName}: Vue;`;
-  })
-  .join('\n');
+const typeFileData =
+  `import Vue from 'vue';` +
+  '\n' +
+  allUtilIndexFilePaths
+    .map(utilIndexFilePath => {
+      const utilName = getParentFolderName(utilIndexFilePath);
+      return `export declare const ${utilName}: Vue;`;
+    })
+    .join('\n');
 
 fs.writeFileSync(
   path.resolve(paths.distFolder, './main.common.d.ts'),
