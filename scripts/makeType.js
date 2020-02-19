de38aa7d825bf1ce7a602cca535199d8234961ea
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-02-19 15:16:50 +0800

    add type

---
+++ scripts/makeType.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra');
+const path = require('path');
+const { allUtilIndexFilePaths, getParentFolderName } = require('./utils');
+const paths = require('./paths');
+
+const typeFileData = `import Vue from 'vue';` + '\n';
+allUtilIndexFilePaths
+  .map(utilIndexFilePath => {
+    const utilName = getParentFolderName(utilIndexFilePath);
+    return `export declare const ${utilName}: Vue;`;
+  })
+  .join('\n');
+
+fs.writeFileSync(
+  path.resolve(paths.distFolder, './main.common.d.ts'),
+  typeFileData
+);
--- scripts/paths.js
+++ scripts/paths.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const paths = {
   srcFolder: path.resolve(__dirname, '../src'),
+  distFolder: path.resolve(__dirname, '../dist'),
   rootIndexFilePath: path.resolve(__dirname, '../src/main.ts')
 };
 
